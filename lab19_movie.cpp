1650e8baff3fe5f90caa56495f0b29ad32cdb9ea
winstonjose01 <winston.jose@gmail.com>
2024-10-06 16:04:51 -0700

    revision-9: adding comments

--- lab19_movie.cpp
+++ lab19_movie.cpp
@@ -8,56 +8,63 @@
 # include <random>
 # include <iomanip>
 using namespace std;
-
-
-
-class Movie {
+// Movie class to store movie reviews
+class Movie {              
     private:
-        const double RATING_MIN = 1.0;
+        // Constants for rating change
+        const double RATING_MIN = 1.0; 
         const double RATING_MAX = 5.0;
-        string movietitle;
+        string movietitle; // Movie title
+        // Struct to represent a node in the linked list (rating, comment)
         struct Node {
-            double val;
-            string comment;
-            Node *next;
-            // Node constructor
+            double val;         // Review rating
+            string comment;     // Review comment
+            Node *next;         // Pointer to next node on the list
+            // Node constructor to initialize the list
             Node (double v, string c){
-                val = v; comment = c; next = nullptr;
+                val = v; comment = c; next = nullptr; 
             }
         };
-        Node * head;
-    public:
+        Node * head;    // Pointer to the head of the linked list
 
+    public: 
+        // Default constructor for Movie object (initializes empty movie title and linked list)
         Movie(){
             movietitle = "";
             head = nullptr;
         }
+
+        // Function to add a new review node at the front of the linked list
         void add_node_front(double rating, string com){
-            Node *newNode = new Node (rating, com);
-            newNode->next = head;
-            head = newNode;
+            Node *newNode = new Node (rating, com);     // Creates a new node
+            newNode->next = head;                       // New node points to the current head
+            head = newNode;                             // Update head to the new node
         }
+
+        // Function to set the movie title
         void settitle(string title){
             movietitle = title;
         }
 
+        // Function to generate a random rating between RATING_MIN and RATING_MAX
         double getrating(){
             random_device seed;
             mt19937 gen(seed());
             uniform_real_distribution<> rating(RATING_MIN, RATING_MAX);
-            return (rating(gen));
-
+            return (rating(gen));   // Return random rating
         }
 
+        // Function to print all reviews for the movie (title and linked list contents)
         void print_reviews(){
             cout << "Reviews for " << movietitle << endl;
+            // Check if the linked list is empty
             if (!head){
                 cout << "Review list is empty.";
                 exit (0);
             }
-            Node *current = head;
+            Node *current = head;   // Start 
             while (current){
-                cout << "\tRating : " << fixed << setprecision(2) << current->val << " | Comment :" << current->comment << endl; 
+                cout << "\tRating : " << fixed << setprecision(1) << current->val << " | Comment :" << current->comment << endl; 
                 current = current->next;
             }
             cout << endl;
