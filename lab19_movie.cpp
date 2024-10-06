92d2affb7772f1a361cdb961cddf44db80012f77
winstonjose01 <winston.jose@gmail.com>
2024-10-06 13:51:30 -0700

    Initial

---
+++ lab19_movie.cpp
@@ -0,0 +1,44 @@
+// COMSC210 | Lab19 | Winston Jose
+// IDE Used: Visual Studio code
+// Github Link:
+
+# include <iostream>
+# include <fstream>
+using namespace std;
+
+
+class Movie {
+    private:
+        string movietitle;
+        struct Node {
+            double val;
+            string comment;
+            Node *next;
+        };
+        Node * head;
+    public:
+
+        Movie(){
+            movietitle = "";
+            head = nullptr;
+
+    }
+
+    
+
+
+
+
+};
+
+
+int main{
+
+
+    return 0;
+}
+
+
+int openfile(fstream &f, string filenames)
+{}
+
